695364ce11068e1b2d1b22eebd6738298b33232a
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-10-17 00:27:51 -0700

    commit tree 13b4895d63a256e185760f4aa64e2ff892321399

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,59 @@
-import React from 'react'
+import React from "react";
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
-      }
-    }
+        type: "all",
+      },
+    };
   }
+
+  handleFilterChange = (event) => {
+    event.persist();
+    this.setState({
+      filters: {
+        type: event.target.value,
+      },
+    });
+  };
+
+  handleFindPetsClick = () => {
+    // fetch list of pets
+    let searchQuery;
+    if (this.state.filters.type === "all") {
+      searchQuery = "";
+    } else {
+      searchQuery = `?type=${this.state.filters.type}`;
+    }
+    fetch(`/api/pets${searchQuery}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        this.setState(() => {
+          return {
+            pets: json,
+          };
+        });
+      })
+      .catch((errors) => {
+        console.log(errors);
+      });
+  };
+
+  handleAdoptedPet = (petId) => {
+    const adoptedPet = this.state.pets.find((pet) => {
+      return (pet.id = petId);
+    });
+    adoptedPet.isAdopted = true;
+  };
 
   render() {
     return (
@@ -24,16 +64,22 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.handleFilterChange}
+                onFindPetsClick={this.handleFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser
+                onAdoptPet={this.handleAdoptedPet}
+                pets={this.state.pets}
+              />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
